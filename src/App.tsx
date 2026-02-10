e7e3baca5f187b39eefb5735b97b8cb61701146d
Fatima Paola Cortes Avendaño <fatiicortes25@gmail.com>
2026-02-09 22:32:06 -0600

    Navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,45 @@
+import { Header } from './components/header'
+
 
 function App() {
 
   return (
-    <>
-    <h1>codingTube</h1>
-    </>
+    <main className= 'border border-white w-[375px]m-auto'>
+      
+      <section className='bg-gray-500 p-6 h-[420px] relative'>
+        <Header />
+        
+        <img
+          src="/images/illustration-intro.png"
+          alt="intro illustration"
+          className='relative z-10 mt-12'
+        />
+        
+        <img
+          className='absolute bottom-0 left-0 w-full xl:hidden'
+          src="/images/bg-curvy-mobile.svg"
+          alt="bg curvy mobile"
+        />
+        <img
+          className='absolute bottom-0 left-0 w-full hidden xl:block'
+          src="/images/bg-curvy-desktop.svg"
+          alt="bg curvy desktop"
+        />
+      </section>
+
+      <section className='bg-Navy-900 p-6 pb-[300px]'>
+        <Hero />
+        <Features />
+        <MoreInformation />
+        <Testimonials />
+      </section>
+
+      <section className='bg-Navy-950 p-6 relative'>
+        <Form />
+        <Footer />
+      </section>
+    
+    </main>
   )
 }
 
